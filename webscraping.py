e59784f415f8b562108cf4a5683f73c9a21d3dcb
kalterdoner99 <kalterdoner@gmail.com>
2024-06-07 15:54:52 +0200

    removed webscraping class because its useless and makes the code more complicate

--- webscraping.py
+++ webscraping.py
@@ -7,113 +7,93 @@ import multiprocessing as mp
 from bs4 import BeautifulSoup
 from selenium_stealth import stealth
 import time
+
 if __name__ == '__main__':
     import headers
 else:
     from . import headers
 import sys
-sys.setrecursionlimit(10000)
 
+sys.setrecursionlimit(10000)
 
 basic_request_header = dict(
     {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    'Accept-Language' : 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
-    'Cache-Control' : 'max-age=0',
-    'Sec-Ch-Ua' : '"Not_A Brand";v="8", "Chromium";v="120", "Opera GX";v="106"',
-    'Sec-Ch-Ua-Mobile' : '?0',
-    'Sec-Ch-Ua-Platform' : '"Windows"',
-    'Sec-Fetch-Dest ' : 'document',
-    'Sec-Fetch-Mode' : 'navigate',
-    'Sec-Fetch-Site' : 'none',
-    'Sec-Fetch-User' : '?1',
-    'Upgrade-Insecure-Requests' : '1',
-    'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0'
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        'Accept-Language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Cache-Control': 'max-age=0',
+        'Sec-Ch-Ua': '"Not_A Brand";v="8", "Chromium";v="120", "Opera GX";v="106"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest ': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
+        'Sec-Fetch-User': '?1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0'
     }
 )
+
+request_info = tuple[str, str, dict, str]
+
 
 def headers_generator():
     for x in headers.list_headers.values():
         yield x
     yield 0
 
-def _request(url:str, keyword:str, headers:MappingProxyType=MappingProxyType({})):
-    generator = False
-    gen = None
-    if headers == MappingProxyType({}):
-        generator = True
-        gen = headers_generator()
-        headers = gen.__next__()
+
+def _request(url: str, keyword: str, headers: dict = None):
+    if headers is None:
+        headers = {}
+    ans = requests.request("get", url=url, headers=headers).text
+    if ans.__contains__(keyword):
+        return ans
+    else:
+        return selenium_requests(url, keyword)
+
+
+def selenium_requests(url: str, keyword: str):
+    options = webdriver.ChromeOptions()
+    options.add_argument("start-maximized")
+    options.add_argument("--headless")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    driver = webdriver.Chrome(options=options)
+    stealth(driver,
+            languages=["en-US", "en"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            )
+    driver.get(url)
+    waiting = 0
     while True:
-        ans = requests.request("get", url=url, headers=headers).text
-        if ans.__contains__(keyword):
-            return ans
-        if generator:
-            headers = gen.__next__()
-            if headers == 0:
-                break
-    return selenium_requests(url, keyword)
+        time.sleep(waiting)
+        source = driver.page_source
+        print(waiting)
+        if source.__contains__(keyword):
+            driver.quit()
+            return BeautifulSoup(source, 'html.parser')
+        if waiting == 40:
+            break
+        waiting += 1
+    driver.quit()
 
-def selenium_requests(url:str, keyword:str):
-    return NotImplemented
 
-os.environ['chromedriver_update'] = 'False'
 
-request_info = tuple[str, str, dict, str]
 
 def func_request(webscraping_class, args):
     return webscraping_class._request(*args)
 
-class webscraping():
 
-    def __init__(self, sql = None):
-        self.sql = sql
-        self.setup()
+def setup(self):
+    if os.environ['chromedriver_update'] == 'False':
+        # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        os.environ['chromedriver_update'] = 'True'
 
-    def setup(self):
-        if os.environ['chromedriver_update'] == 'False':
-            #driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-            os.environ['chromedriver_update'] = 'True'
-
-    def _request(self, url:str, method:str='get', headers:dict=None, keyword:str= ''):
-        if headers is None:
-            headers = basic_request_header
-        ans = requests.request(method, url=url, headers=headers).text
-        soup = BeautifulSoup(ans, 'html.parser')
-        if keyword == '':
-            return soup
-        elif ans.__contains__(keyword):
-            return soup
-        else:
-            options = webdriver.ChromeOptions()
-            options.add_argument("start-maximized")
-            options.add_argument("--headless")
-            options.add_experimental_option("excludeSwitches", ["enable-automation"])
-            options.add_experimental_option('useAutomationExtension', False)
-            driver = webdriver.Chrome(options=options)
-            stealth(driver,
-                    languages=["en-US", "en"],
-                    vendor="Google Inc.",
-                    platform="Win32",
-                    webgl_vendor="Intel Inc.",
-                    renderer="Intel Iris OpenGL Engine",
-                    fix_hairline=True,
-                    )
-            driver.get(url)
-            waiting = 0
-            while True:
-                time.sleep(waiting)
-                source = driver.page_source
-                print(waiting)
-                if source.__contains__(keyword):
-                    driver.quit()
-                    return BeautifulSoup(source, 'html.parser')
-                if waiting == 40:
-                    break
-                waiting += 1
-            driver.quit()
-
-    def request(self, request_info_:list[request_info], process:int=1):
+    def request(self, request_info_: list[request_info], process: int = 1):
         for i, single_info in enumerate(request_info_):
             new_list = [self]
             new_list.append(single_info)
@@ -123,11 +103,11 @@ class webscraping():
         res = self._run(process=process, func=func_request, args=request_info_)
         print(res)
 
-    def _run(self,process:int, func, args:iter):
-        with mp.Pool(processes = process) as pool:
+    def _run(self, process: int, func, args: iter):
+        with mp.Pool(processes=process) as pool:
             return pool.starmap(func, args)
 
-    def get_in_string(self, string:str, Str1, Str2):
+    def get_in_string(self, string: str, Str1, Str2):
         if string.find(Str1) != -1:
             string = string[string.find(Str1) + len(Str1):]
             string = string[:string.find(Str2)]
@@ -135,11 +115,13 @@ class webscraping():
         else:
             return False
 
+
 def test(arg):
     time.sleep(3)
     return arg
 
+
 if __name__ == '__main__':
     c = webscraping()
-    a:request_info = ('https://www.ecosia.org/?c=de', 'get', basic_request_header, '')
+    a: request_info = ('https://www.ecosia.org/?c=de', 'get', basic_request_header, '')
     print(c.request([a, a]))
